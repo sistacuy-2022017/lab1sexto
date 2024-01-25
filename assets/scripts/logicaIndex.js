7d265eced9ea753555bee2a0e73bead95a7885f0
Informatica <informatica@alumno.local>
2024-01-25 11:56:39 -0600

    subiendo cambios

--- assets/scripts/logicaIndex.js
+++ assets/scripts/logicaIndex.js
@@ -1,32 +1,3 @@
-async function enviarDatos(){
-    const nombre = document.getElementById('btn').value;
- 
-    try{
-        const resultado = await enviarDatosAlServidor(nombre);
-        mensajeAgradecimiento(resultado);
-    }catch(e){
-        mensajeError(e);
-    }
-}
-
-
-function enviarDatosAlServidor(nombre){
-    return new Promise((resolve,reject) => {
-        
-        setTimeout(() =>{
-            const exito = nombre.toLowerCase() !== 'error';
-            
-            const resultado = exito ? 'https://www.formula1.com/' : 'Datos erroneos';
-            exito ? resolve(resultado) : reject(resultado);
-
-
-        }, 2000);
-
-    });
-}
-
-function mensajeAgradecimiento(mensaje){
-    const mensajeElement = document.getElementById('btn');
-    mensajeElement.textContent = mensaje;
-    mensajeElement.classList.remove('oculto');
+function redirect(){
+    window.location.assign('https://www.formula1.com/');
 }
